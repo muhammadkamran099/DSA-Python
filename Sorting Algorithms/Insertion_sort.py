e88ef6b57e04a2d9e1c1376865431df0f457314b
learning-log <muhammadkamran2599@gmail.com>
2026-07-06 22:55:55 +0500

    Sorting Algorithms

---
+++ Sorting Algorithms/Insertion_sort.py
@@ -0,0 +1,11 @@
+nums = [ 3, 4, 5, 6, 8, 9, 10, 7,  1]
+n = len(nums)
+for i in range(1, n):
+    key = nums[i]
+    j = i-1
+    while j>= 0 and key < nums[j]:
+        nums[j+1] = nums[j]
+        j -= 1
+    nums[j+1] = key
+    
+print(nums)
