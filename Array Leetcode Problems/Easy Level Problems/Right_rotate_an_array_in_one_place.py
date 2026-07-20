dc4fbdca90172dcceb9cc747d23f056ea72e83ca
learning-log <muhammadkamran2599@gmail.com>
2026-07-20 20:54:07 +0500

    Standard in-place right rotation by one position

--- Array Leetcode Problems/Easy Level Problems/Right_rotate_an_array_in_one_place.py
+++ Array Leetcode Problems/Easy Level Problems/Right_rotate_an_array_in_one_place.py
@@ -1,9 +1,22 @@
 
 """Brute force"""
 
+# nums = [7, 5, 3, -2, -6, 8, 4]
+
+# n = len(nums)
+# nums[:] = nums[n-1:] + nums[0:n-1]
+
+# print(nums)
+
+""" Standard in-place right rotation by one position """
+
 nums = [7, 5, 3, -2, -6, 8, 4]
 
 n = len(nums)
-nums[:] = nums[n-1:] + nums[0:n-1]
+temp = nums[n-1]
+for i in range(n-2, -1, -1):
+    nums[i+1] = nums[i]
+nums[0] = temp
 
 print(nums)
+    
