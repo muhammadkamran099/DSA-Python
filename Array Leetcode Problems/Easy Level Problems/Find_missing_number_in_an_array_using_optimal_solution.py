2600e3873d653608def3568b04cb4fb65f9f12b2
learning-log <muhammadkamran2599@gmail.com>
2026-08-21 00:22:52 +0500

    Find_missing_number_in_an_array_using_optimal_solution

---
+++ Array Leetcode Problems/Easy Level Problems/Find_missing_number_in_an_array_using_optimal_solution.py
@@ -0,0 +1,24 @@
+def sum_n(n):
+
+    return n * (n + 1) // 2
+
+
+def find_missing_number(nums):
+
+    n = len(nums)
+
+    expected_sum = sum_n(n)
+
+    actual_sum = 0
+
+    for num in nums:
+        actual_sum += num
+
+    return expected_sum - actual_sum
+
+
+nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+
+result = find_missing_number(nums)
+
+print(result)
