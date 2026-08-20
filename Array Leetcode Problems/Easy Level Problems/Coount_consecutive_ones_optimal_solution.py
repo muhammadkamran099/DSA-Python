7b8d0e97caaff83c0bf256460569e02d7dbb6925
learning-log <muhammadkamran2599@gmail.com>
2026-08-21 00:34:53 +0500

    Coount_consecutive_ones_optimal_solution

---
+++ Array Leetcode Problems/Easy Level Problems/Coount_consecutive_ones_optimal_solution.py
@@ -0,0 +1,21 @@
+def max_consecutive_ones(nums):
+    count = 0
+    max_count = 0
+
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            count += 1
+        else:
+            max_count = max(max_count, count)
+            count = 0
+
+    max_count = max(max_count, count)
+
+    return max_count
+
+
+nums = [1, 1, 0, 1, 1, 1, 1, 0, 1, 1]
+
+result = max_consecutive_ones(nums)
+
+print(result)
