02971531e3c1faff831fb511980d698c03eeba9b
learning-log <muhammadkamran2599@gmail.com>
2026-08-20 12:01:17 +0500

    Move_zeros_to_the_end_of_List_brute_force

---
+++ Array Leetcode Problems/Easy Level Problems/Move_zeros_to_the_end_of_List_brute_force.py
@@ -0,0 +1,26 @@
+def move_zeros(nums):
+    
+    n = len(nums)
+
+    temp = []
+
+    for i in range(n):
+        if nums[i] != 0:
+            temp.append(nums[i])
+
+    tn = len(temp)
+
+    for i in range(tn):
+        nums[i] = temp[i]
+
+    for i in range(tn, n):
+        nums[i] = 0
+
+    return nums
+
+
+nums = [1, 0, 2, 4, 3, 0, 0, 3, 5, 1]
+
+result = move_zeros(nums)
+
+print(result)
