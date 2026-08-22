6d8d4426d79a1b4bfb6168ff6c62cdd1d02aee04
learning-log <muhammadkamran2599@gmail.com>
2026-08-22 15:51:21 +0500

    Two_Sum_Problem_using_brute_force

---
+++ Array Leetcode Problems/Medium Level Problems/Two_Sum_Problem_using_brute_force.py
@@ -0,0 +1,17 @@
+
+
+def TwoSum(nums, target):
+    n = len(nums)
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return -1
+
+nums = [5, 9, 1, 2, 4, 15, 6, 3]
+
+target = 13
+
+result = TwoSum(nums, target)
+
+print(result)
