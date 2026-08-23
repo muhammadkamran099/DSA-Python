66543d2b06e52b55845ea74e575bd99027400df9
learning-log <muhammadkamran2599@gmail.com>
2026-08-23 23:25:38 +0500

    Maximum_Sub_Array_brute_force

--- Array Leetcode Problems/Medium Level Problems/Maximum_Sub_Array_brute_force.py
+++ Array Leetcode Problems/Medium Level Problems/Maximum_Sub_Array_brute_force.py
@@ -1,2 +1,19 @@
+def max_sub_array(nums):
+    n = len(nums)
+    max_sum = float("-inf")
+
+    for i in range(n):
+        total = 0
+
+        for j in range(i, n):
+            total += nums[j]
+            max_sum = max(max_sum, total)
+
+    return max_sum
+
+
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
+result = max_sub_array(nums)
+
+print(result)
