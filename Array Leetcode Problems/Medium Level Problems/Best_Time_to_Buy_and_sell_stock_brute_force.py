d32a56eebc4b80e82ff78dcdafa2543147f137d7
learning-log <muhammadkamran2599@gmail.com>
2026-08-24 21:37:36 +0500

    Best_Time_to_Buy_and_sell_stock_brute_force

--- Array Leetcode Problems/Medium Level Problems/Best_Time_to_Buy_and_sell_stock_brute_force.py
+++ Array Leetcode Problems/Medium Level Problems/Best_Time_to_Buy_and_sell_stock_brute_force.py
@@ -1,2 +1,16 @@
+def best_time_to_buy_and_sell(prices):
+    max_profit = 0
+
+    for i in range(len(prices) - 1):
+        for j in range(i + 1, len(prices)):
+            total_profit = prices[j] - prices[i]
+            max_profit = max(max_profit, total_profit)
+
+    return max_profit
+
+
 prices = [7, 2, 1, 5, 6, 4, 8]
 
+result = best_time_to_buy_and_sell(prices)
+
+print(result)
