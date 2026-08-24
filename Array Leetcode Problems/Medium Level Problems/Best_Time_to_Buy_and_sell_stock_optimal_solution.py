241e613721c8342b680af422c275a133a73c00d0
learning-log <muhammadkamran2599@gmail.com>
2026-08-24 23:34:35 +0500

    Best_Time_to_Buy_and_sell_stock_optimal_solution

--- Array Leetcode Problems/Medium Level Problems/Best_Time_to_Buy_and_sell_stock_optimal_solution.py
+++ Array Leetcode Problems/Medium Level Problems/Best_Time_to_Buy_and_sell_stock_optimal_solution.py
@@ -1,1 +1,16 @@
+def best_time_to_buy_and_sell(prices):
+    max_profit = 0
+    min_price = float("inf")
+
+    for price in prices:
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+
 prices = [7, 2, 1, 5, 6, 4, 8]
+
+result = best_time_to_buy_and_sell(prices)
+
+print(result)
