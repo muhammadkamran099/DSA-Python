a51d80bfe2d3dab9057f07e56a299dc8aaa22c5b
learning-log <muhammadkamran2599@gmail.com>
2026-08-28 21:25:43 +0500

    Add rearrange array by sign solution

--- Array Leetcode Problems/Medium Level Problems/Rearrange_Array_Element_by_Sign_optimal_solution.py
+++ Array Leetcode Problems/Medium Level Problems/Rearrange_Array_Element_by_Sign_optimal_solution.py
@@ -1,4 +1,23 @@
+def rearrange_by_sign(nums):
+    n = len(nums)
+    result = [0] * n
+
+    pos_idx = 0
+    neg_idx = 1
+
+    for num in nums:
+        if num >= 0:
+            result[pos_idx] = num
+            pos_idx += 2
+        else:
+            result[neg_idx] = num
+            neg_idx += 2
+
+    return result
+
+
 nums = [5, 10, -3, -1, -10, 6]
 
-n = len(nums)
+result = rearrange_by_sign(nums)
 
+print(result)
