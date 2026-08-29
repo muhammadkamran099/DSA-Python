fefadd0ae2b0be81e51f2af642692149028c8e4c
learning-log <muhammadkamran2599@gmail.com>
2026-08-29 23:10:07 +0500

    Add longest consecutive sequence solution

---
+++ Array Leetcode Problems/Medium Level Problems/Longest_consecutive_sequence_brute_force.py
@@ -0,0 +1,22 @@
+def longest_consecutive(nums):
+    n = len(nums)
+    max_count = 0
+
+    for i in range(n):
+        num = nums[i]
+        count = 1
+
+        while num + 1 in nums:
+            count += 1
+            num += 1
+
+        max_count = max(max_count, count)
+
+    return max_count
+
+
+nums = [1, 99, 101, 98, 2, 5, 3, 100, 1, 1]
+
+result = longest_consecutive(nums)
+
+print(result)
