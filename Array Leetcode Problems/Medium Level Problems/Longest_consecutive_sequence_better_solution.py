36667d5ba797d0ad23cdfefeff3069f3ab49abd4
learning-log <muhammadkamran2599@gmail.com>
2026-09-07 20:22:06 +0500

    Longest Consecutive Sequence

--- Array Leetcode Problems/Medium Level Problems/Longest_consecutive_sequence_better_solution.py
+++ Array Leetcode Problems/Medium Level Problems/Longest_consecutive_sequence_better_solution.py
@@ -1,1 +1,27 @@
+def longest_consecutive(nums):
+    nums.sort()
+    
+    count = 0
+    last_smaller = float("-inf")
+    longest = 0
+
+    for num in nums:
+        if num == last_smaller + 1:
+            count += 1
+            last_smaller = num
+        elif num == last_smaller:
+            continue
+        else:
+            count = 1
+            last_smaller = num
+
+        longest = max(longest, count)
+
+    return longest
+
+
 nums = [1, 99, 101, 98, 2, 5, 3, 100, 1]
+
+result = longest_consecutive(nums)
+
+print(result)
