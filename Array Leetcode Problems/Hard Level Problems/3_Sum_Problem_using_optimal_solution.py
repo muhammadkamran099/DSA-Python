adbe3d712cd373c205c3452354de650114d6be11
learning-log <muhammadkamran2599@gmail.com>
2026-09-11 22:01:07 +0500

    Add 3Sum logic using hash sets

---
+++ Array Leetcode Problems/Hard Level Problems/3_Sum_Problem_using_optimal_solution.py
@@ -0,0 +1,17 @@
+def find_triplets(arr):
+    n = len(arr)
+    result = set()
+    
+    for i in range(0, n):
+        my_set = set()
+        for j in range(i + 1, n):
+            third = -(arr[i] + arr[j])
+            
+            if third in my_set:
+                temp = [arr[i], arr[j], third]
+                temp.sort()
+                result.add(tuple(temp))
+                
+            my_set.add(arr[j])
+            
+    return list(result)
