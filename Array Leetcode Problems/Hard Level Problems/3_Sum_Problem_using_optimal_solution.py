a06a73bcc064852c436f42ed43725a6d59a889cd
learning-log <muhammadkamran2599@gmail.com>
2026-09-14 00:08:18 +0500

    Solve three sum with two pointers

--- Array Leetcode Problems/Hard Level Problems/3_Sum_Problem_using_optimal_solution.py
+++ Array Leetcode Problems/Hard Level Problems/3_Sum_Problem_using_optimal_solution.py
@@ -1,18 +1,33 @@
-# Better Solution
-def find_triplets(arr):
-    n = len(arr)
-    result = set()
-    
-    for i in range(0, n):
-        my_set = set()
-        for j in range(i + 1, n):
-            third = -(arr[i] + arr[j])
-            
-            if third in my_set:
-                temp = [arr[i], arr[j], third]
-                temp.sort()
-                result.add(tuple(temp))
-                
-            my_set.add(arr[j])
-            
-    return list(result)
+def threeSum(nums):
+    ans = []
+    n = len(nums)
+    nums.sort()
+
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        j = i + 1
+        k = n - 1
+
+        while j < k:
+            total_sum = nums[i] + nums[j] + nums[k]
+
+            if total_sum < 0:
+                j += 1
+
+            elif total_sum > 0:
+                k -= 1
+
+            else:
+                ans.append([nums[i], nums[j], nums[k]])
+                j += 1
+                k -= 1
+
+                while j < k and nums[j] == nums[j - 1]:
+                    j += 1
+
+                while j < k and nums[k] == nums[k + 1]:
+                    k -= 1
+
+    return ans
