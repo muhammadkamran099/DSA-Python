fb316fb4fca7fabe48fa9d1e689649458ce48cff
learning-log <muhammadkamran2599@gmail.com>
2026-09-19 15:44:17 +0500

    Search in Rotated Sorted Array II

---
+++ Binary Search/Search_in_Rotated_Sorted_Array_II_optimal_solution.py
@@ -0,0 +1,33 @@
+def search_rotated_sorted_array(nums, target):
+    n = len(nums)
+    low = 0
+    high = n - 1
+    is_true = False
+    while low <= high:
+        mid = (low + high) // 2
+
+        if nums[mid] == target:
+            is_true = True
+            return is_true
+        if nums[low] == nums[mid] == nums[high]:
+            low += 1
+            high -= 1
+            continue
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target <= nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        else:
+            if nums[mid] <= target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+    return is_true
+
+nums = [1, 0, 1, 1, 1]
+target = 0
+result = search_rotated_sorted_array(nums, target)
+print(result)
