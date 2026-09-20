d6dfbdb2cf2d197e386f5e1ec8d1f7485ca121cd
learning-log <muhammadkamran2599@gmail.com>
2026-09-20 18:40:59 +0500

    Remove Outermost Paranthesis

---
+++ Strings/Remove_Outermost_paranthesis.py
@@ -0,0 +1,14 @@
+s = "(()())(())"
+
+count = 0
+result = ""
+for ch in s:
+    if ch == "(":
+        count += 1
+        if count > 1:
+            result += ch
+    else:
+        count -= 1
+        if count > 0:
+            result += ch
+print(result)
